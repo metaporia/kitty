3ca83622ccc6229f28a99d98ceac61ae51632656
metaporia <kyahnkrafft@me.com>
2019-08-07 23:52:47 -0700

    CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,46 @@
 module Main where
 
-import Lib
+import Lib (parseRows)
+import qualified Text.Trifecta as Tri
+import Text.Show.Pretty (pPrint, ppShow)
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+
+data Source = FilePath String 
+             | StdIn
+             | Bare
+             deriving (Eq, Show)
+
+
+filePath :: Parser Source
+filePath =
+  FilePath <$>
+  option str (metavar "FILE" <> long "input" <> short 'i' <> help "Parse from file.")
+
+-- | Always succeeds.
+stdIn :: Parser Source
+stdIn =
+  const StdIn <$>
+  flag'  StdIn (long "stdin" <> short 's' <> help "Parse from stdin.")
+
+dispatch :: Source -> IO ()
+dispatch source = do
+  let displayTally = pPrint . parseRows ["a", "k", "l"]
+  case source of
+    (FilePath fp) -> readFile fp >>= displayTally
+    StdIn -> getContents >>= displayTally
+    Bare -> putStrLn version
+
+toplevel :: Parser Source
+toplevel =
+  stdIn <|> filePath <|>
+  (infoOption version (long "version" <> short 'V') <*> pure Bare) <|> pure Bare -- VERSION
+
+-- VERSION
+version = "kitty 0.0.1"
 
 main :: IO ()
-main = putStrLn "kitty -- Parse kitty csv files."
+main = execParser opts >>= dispatch 
+  where headerLn = "kitty -- Parse & summarize kitty csv files."
+        opts  = info (helper <*> toplevel) (fullDesc <> header headerLn)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,78 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
-module Lib where
+{-# LANGUAGE OverloadedStrings, RecordWildCards,
+  ScopedTypeVariables, QuasiQuotes #-}
 
-import qualified Text.Trifecta as Tri
+module Lib (rows, parseRows) where
+
 import Control.Applicative
-import Text.Show.Pretty (pPrint, ppShow)
+import Control.Monad
+import Data.Foldable (asum)
+import Text.Parser.Combinators
 import Text.RawString.QQ
+import Text.Read (readEither)
+import Text.Show.Pretty (pPrint, ppShow)
+import qualified Text.Trifecta as Tri
+import Text.Trifecta
 
-x = 3
+-- <dollars-spent>,<participated>,<paid>
+exCsv =
+  [r|30,kl,k
+120,akl,l
+35,al,a
+300,l,k
+|]
+
+exRow = "120,akl,l"
+
+price :: Parser (Either String Float)
+price = do
+  readEither <$> (some digit <?> "price expected to be only digits")
+
+-- | The input list should consist of unique alphanumeric identifiers.
+identifier ::
+     [String] -- | ids
+  -> Parser String
+identifier
+  -- FIXME assert uniqueness
+  --xs <- oneOf (string <$> ids)
+ = do
+  choice . fmap (\i -> string i <?> "unexpected identifier: \n" ++ i)
+
+ourIdentifier :: Parser String
+ourIdentifier = identifier ["a", "k", "l"]
+
+ourIds = ["a", "k", "l"]
+
+ourRow = row ["a", "k", "l"]
+
+data Row = Row
+  { dollars :: Float
+  , participants :: [String]
+  , payers :: [String]
+  } deriving (Eq, Show)
+
+row :: [String] -> Parser Row
+row ids = do
+  let collectIds = some $ identifier ids
+      sep = char ',' <* skipOptional space
+  eitherDollars <- price
+  dollars <-
+    case eitherDollars of
+      Left err -> fail (ppShow err)
+      Right dollars -> return dollars
+  _ <- sep
+  participants <- collectIds
+  _ <- sep
+  payers <- collectIds
+  return $ Row {..}
+
+-- | Parse kitty csv file.
+rows :: [String] -> Parser [Row]
+rows ids = (row ids  `sepEndBy` newline) <* eof
+
+parseRows :: [String] -> String -> Result [Row]
+parseRows ids = test (rows ids) 
+
+ourRows = rows ourIds
+
+test :: Parser a -> String -> Result a
+test parser = parseString parser mempty
